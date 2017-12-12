56a22e4704435cd4813d54991b3210bf5359c9b6
jjarquio <jaysonjarquio@yahoo.com>
2017-12-13 02:44:36 +0800

    db

--- rassi_pos (1).sql
+++ rassi_pos (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2017 at 07:23 AM
+-- Generation Time: Dec 12, 2017 at 06:20 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -45,8 +45,17 @@ CREATE TABLE `products` (
   `product_price` decimal(15,2) UNSIGNED NOT NULL,
   `product_description` text,
   `product_model` varchar(200) NOT NULL,
-  `product_brand` int(11) NOT NULL
+  `product_brand` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `product_code`, `product_quantity`, `product_price`, `product_description`, `product_model`, `product_brand`) VALUES
+(1, 'rwer', 'qreqweqw', 9987, '123123.00', 'werwe', '2312312', '123123123'),
+(2, 'Laptop acer', '1231kdasidwe', 1221, '12312312.00', 'bsta acer', '123129485', '1239585dasd'),
+(3, 'LOYOLA', 'qweqw3123', 196, '1111.00', 'qwe', '1239iewh', 'jeiqe1');
 
 -- --------------------------------------------------------
 
@@ -58,9 +67,25 @@ CREATE TABLE `sale` (
   `sale_id` int(8) UNSIGNED ZEROFILL NOT NULL,
   `sale_cashier` int(10) UNSIGNED NOT NULL,
   `sale_cash` decimal(15,2) UNSIGNED NOT NULL,
-  `sale_time` datetime NOT NULL,
-  `sale_dine` bit(1) NOT NULL DEFAULT b'1'
+  `sale_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sale`
+--
+
+INSERT INTO `sale` (`sale_id`, `sale_cashier`, `sale_cash`, `sale_time`) VALUES
+(00000001, 1, '123123.00', '2017-12-16 00:00:00'),
+(00000002, 3, '123123.00', '2017-12-08 00:00:00'),
+(00000003, 1, '1111.00', '2017-12-09 21:16:19'),
+(00000004, 1, '12312312.00', '2017-12-09 22:22:19'),
+(00000005, 1, '24995104.00', '2017-12-10 02:23:40'),
+(00000006, 1, '123123.00', '2017-12-10 18:13:54'),
+(00000007, 1, '123246243.00', '2017-12-10 19:53:12'),
+(00000008, 1, '123123.00', '2017-12-10 20:11:42'),
+(00000009, 1, '123123.00', '2017-12-10 20:25:43'),
+(00000010, 1, '369369.00', '2017-12-11 03:44:48'),
+(00000011, 1, '246246.00', '2017-12-12 16:26:20');
 
 -- --------------------------------------------------------
 
@@ -74,6 +99,14 @@ CREATE TABLE `sale_item` (
   `prod_qty` int(10) UNSIGNED NOT NULL,
   `prod_price` decimal(15,2) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sale_item`
+--
+
+INSERT INTO `sale_item` (`sale_id`, `prod_id`, `prod_qty`, `prod_price`) VALUES
+(00000001, 1, 1, '123123.00'),
+(00000002, 3, 1, '123123.00');
 
 -- --------------------------------------------------------
 
@@ -104,10 +137,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `passcode`, `user_type`) VALUES
-(1, 'James Ginete', '12345', b'0'),
-(3, 'Jayson Jarquio', '12345', b'1'),
-(4, 'Vann Amarillo', '67890', b'0'),
-(6, 'Ira Gamban', '67890', b'1');
+(1, 'James Ginete', '1234', b'0'),
+(3, 'jaysonjarquio', '12345', b'1'),
+(4, 'LOYOLA', '12345', b'1'),
+(5, 'mina', '12345', b'0');
 
 --
 -- Indexes for dumped tables
@@ -161,17 +194,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `sale`
 --
 ALTER TABLE `sale`
-  MODIFY `sale_id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `sale_id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
